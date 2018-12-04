dc7293027535e3a7afd9c2b8d83a708e69e016ab
Alexboss2 <alex520p@edu.easj.dk>
2018-12-04 10:02:12 +0100

    Class resturant

---
+++ LocalDinner/Restaurants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDinner
+{
+    class Restaurants
+    {
+        public string Name { get; set; }
+        public string By { get; set; }
+        public string Køkkentype { get; set; }
+        public int AntalPladser { get; set; }
+    }
+}
