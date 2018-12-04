d3b1ce026c9eca15fd7d9a47051345866d046ae5
Alexboss2 <alex520p@edu.easj.dk>
2018-12-04 10:32:41 +0100

    implentet resaurants class

---
+++ LocalDinner/Model/Restaurant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDinner
+{
+    class Restaurant
+    {
+        #region Properties
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Category { get; set; }
+        public int NumberOfSeats { get; set; }
+        public double OpeningHours { get; set; }
+        #endregion
+
+        #region Constructor
+        
+        public Restaurant(string name, string city, string category, int numberOfSeats, double openingHours)
+        {
+            Name = name;
+            City = city;
+            Category = category;
+            NumberOfSeats = numberOfSeats;
+            OpeningHours = openingHours;
+        }
+        #endregion 
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(City)}: {City}, {nameof(Category)}: {Category}, {nameof(NumberOfSeats)}: {NumberOfSeats}, {nameof(OpeningHours)}: {OpeningHours}";
+        }
+    }
+}
