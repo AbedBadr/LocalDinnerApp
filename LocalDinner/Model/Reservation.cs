d66de802ee9d1a2bf055e8e4e7ad95cef4498aef
AbxdHxcks <abedbadr1999@hotmail.com>
2018-12-06 10:36:35 +0100

    Added reservation class

---
+++ LocalDinner/Model/Reservation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDinner.Model
+{
+    class Reservation
+    {
+        #region Instance Fields
+
+        private int _bookingNr = 0;
+        #endregion
+
+        #region Properties
+
+        public DateTime Dato { get; set; }
+        public int BookingNr
+        {
+            get { return _bookingNr; }
+            set { _bookingNr = value; }
+        }
+        public int AntalPladser { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public Reservation(int antalPladser)
+        {
+            Dato = DateTime.Today;
+            BookingNr = BookingNr++;
+            AntalPladser = antalPladser;
+        }
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+    }
+}
