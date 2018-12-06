834e431c2895629feb368647b0f9b2c8ddac6df7
Maherkhachehan <36763289+Maherkhachehan@users.noreply.github.com>
2018-12-06 10:11:58 +0100

    Test

---
+++ LocalDinner/Model/Calendar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDinner.Model
+{
+    class Calendar
+    {
+        public DateTime Dato { get; set; }
+        public int AktuelAntalPladser { get; set; }
+  
+    }
+}
